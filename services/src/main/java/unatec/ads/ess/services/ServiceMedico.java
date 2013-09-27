aa350361b7414fde452b65454e7c21479333b240
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2013-09-27 00:51:27 -0300

    Versão com Modelo e serviços

--- services/src/main/java/unatec/ads/ess/services/ServiceMedico.java
+++ services/src/main/java/unatec/ads/ess/services/ServiceMedico.java
@@ -4,10 +4,29 @@
  */
 package unatec.ads.ess.services;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
 /**
  *
  * @author emilianoeloi
  */
+@Path("medicos")
 public class ServiceMedico {
+    
+    @Context
+    UriInfo uriInfo;
+    @Context
+    Request request;
+    
+    String id;
+    
+    public ServiceMedico(UriInfo uriInfo, Request request, String id){
+        this.uriInfo = uriInfo;
+        this.request = request;
+        this.id = id;
+    }
     
 }
