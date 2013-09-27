5f8d361dc2fc168b89cbc637ae5cb78e06848e5e
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2013-09-27 00:50:57 -0300

    Versão com Modelo e serviços

--- model/src/main/java/unatec/ads/ess/model/Medico.java
+++ model/src/main/java/unatec/ads/ess/model/Medico.java
@@ -4,18 +4,26 @@
  */
 package unatec.ads.ess.model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  *
  * @author emilianoeloi
  */
+@DatabaseTable(tableName = "medico")
 public class Medico extends Pessoa {
 
+        @DatabaseField(id = true)
 	private String crm;
 
+        @DatabaseField
 	private String nome;
 
+        @DatabaseField
 	private String email;
 
+        @DatabaseField
 	private String telefone;
 
 }
