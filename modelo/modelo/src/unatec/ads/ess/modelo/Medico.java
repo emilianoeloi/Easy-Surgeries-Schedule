fecd6b4ab6e81b45a0a06aab12de9872489091f4
Emiliano Eloi <emilianoeloi@gmail.com>
2013-10-21 20:23:08 -0200

    PAcote

---
+++ modelo/modelo/src/unatec/ads/ess/modelo/Medico.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unatec.ads.ess.modelo;
+
+/**
+ *
+ * @author USER
+ */
+public class Medico {
+    
+}
